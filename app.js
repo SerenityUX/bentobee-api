dfae6df49bbca79a514337947b881a3fbd46a532
Serenidad <95105859+SerenityUX@users.noreply.github.com>
2022-09-03 16:59:27 -0400

    API Inital Test

---
+++ app.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import dotenv from "dotenv"
+import mysql from 'mysql2/promise'
+
+dotenv.config();
+
+const app = express();
+
+const connection = await mysql.createConnection(process.env.DATABASE_URL);
+
+app.get('/', (req, res) => {
+    res.json({msg: 'Hello world'})
+})
+
+app.get('/characters', async(req, res) => {
+    try {
+        const query = "SELECT * FROM hp_character";
+        const [rows] = await connection.query(query)
+        res.send(rows);        
+    } catch(err) {
+        console.error(err)
+    }
+})
+
+app.get('/characters/:id', async (req, res) => {
+    let status = 200;
+    let retVal = {};
+    const {id} = req.params;
+
+    try {
+        const query = "SELECT * FROM hp_character WHERE hp_character.id=?"
+        const [rows] = await connection.query(query, [id])
+        retVal.data = rows
+    } catch(err) {
+        console.error(err)
+        status = 500;
+        retVal = err;
+    } finally {
+        res.status(status).json(retVal)
+    }
+})
+
+app.listen(3001, () => {
+    console.log("App is listening")
+})
